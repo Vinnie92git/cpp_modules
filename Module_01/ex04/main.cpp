378bfde5d73e9bdd5bad2bec051c6d25889a7e45
Vicente <vicente_mad@hotmail.com>
2024-05-13 21:20:14 +0200

    started module_01 ex04

---
+++ Module_01/ex04/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vini <vini@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 15:00:42 by vini              #+#    #+#             */
+/*   Updated: 2024/05/13 21:19:35 by vini             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cerr << "Wrong number of arguments." << std::endl;
+		return (1);
+	}
+	return (0);
+}
