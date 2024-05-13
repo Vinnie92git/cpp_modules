732e4d43bdb3dc640b3395dfaa7d12e4c7d52ecb
Vicente <vicente_mad@hotmail.com>
2024-05-14 00:26:05 +0200

    minor changes

--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vini <vini@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 15:00:42 by vini              #+#    #+#             */
-/*   Updated: 2024/05/13 21:19:35 by vini             ###   ########.fr       */
+/*   Updated: 2024/05/13 22:53:41 by vini             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,15 @@ int	main(int argc, char **argv)
 		std::cerr << "Wrong number of arguments." << std::endl;
 		return (1);
 	}
+
+	std::ifstream	infile(argv[1]);
+	if (!infile.is_open())
+	{
+		std::cerr << "Error opening file." << std::endl;
+		return (1);
+	}
+
+	std::string	content;
+	std::getline(infile, content, '\0');
 	return (0);
 }
