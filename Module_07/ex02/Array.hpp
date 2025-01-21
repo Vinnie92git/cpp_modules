cf49110cab3eedcb027a2b9c5c3c9b7c6a2abd17
Vicente <vicente_mad@hotmail.com>
2025-01-22 00:49:30 +0100

    module07 ex02 almost finished

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: vini <vini@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 20:31:49 by vini              #+#    #+#             */
-/*   Updated: 2025/01/20 20:44:32 by vini             ###   ########.fr       */
+/*   Updated: 2025/01/21 22:45:26 by vini             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,60 @@ template<typename T>
 class Array
 {
 public:
-	Array();
-	Array(unsigned int n);
-	Array(Array& toCopy);
-	Array& operator=(const Array& toAssign);
-	~Array();
+	Array()
+	{
+		content = new T[0];
+		arrayLen = 0;
+	}
 
-	size_t	size();
+	Array(unsigned int n)
+	{
+		content = new T[n];
+		arrayLen = n;
+	}
+
+	Array(Array& toCopy)
+	{
+		// arrayLen = toCopy.size();
+		// content = new T[arrayLen];
+		// for (int i = 0; i < arrayLen; i++)
+		// 	content[i] = toCopy.content[i];
+		*this = toCopy;
+	}
+
+	Array& operator=(const Array& toAssign)
+	{
+		if (this != toAssign)
+		{
+			delete[] content;
+			arrayLen = toAssign.size();
+			content = new T[arrayLen];
+			for (int i = 0; i < arrayLen; i++)
+				content[i] = toAssign.content[i];
+		}
+		return *this;
+	}
+
+	T& operator[](int i)
+	{
+		if (i < 0 || i >= arrayLen)
+			throw std::out_of_range("Error: index is out of bounds");
+		return content[i];
+	}
+
+	~Array()
+	{
+		delete[] content;
+	}
+
+	int	size()
+	{
+		return arrayLen;
+	}
+
 private:
-
+	T*	content;
+	int	arrayLen;
 };
 
 #endif
